b2537231981e49b1b8c4db6c660cf5b606afe62a
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-12 11:40:54 +0200

    bucle for en panel usuarios

--- src/app/components/usuarios-roles/usuarios-roles.ts
+++ src/app/components/usuarios-roles/usuarios-roles.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { Buscador } from '../../shared/buscador/buscador';
 import { RouterLink } from '@angular/router';
+import { UsersService } from '../../services/users-service';
+import { IUsuario } from '../../interfaces/i-usuario';
 
 @Component({
   selector: 'app-usuarios-roles',
@@ -9,5 +11,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './usuarios-roles.css',
 })
 export class UsuariosRoles {
+  mensaje: string = '';
+  tipo: boolean = false;
+  usuarios: IUsuario[] = [];
+  usersService = inject(UsersService);
+  iniciales:string='';
 
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngOnInit(){
+    this.usersService.getAllUsers().subscribe((data) => {
+      if (data.error) {
+        this.mensaje = data.error;
+        return;
+      } else {
+        console.log(data);
+        this.usuarios = data;
+        this.cd.detectChanges();
+      }
+    });
+  }
 }
