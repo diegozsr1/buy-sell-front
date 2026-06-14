b7fd1447af81a65e9892bbdb0aa38729975ccabc
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-14 15:47:30 +0200

    feat(config): change variants of button-icons

--- src/app/shared/atoms/button-icon/button-icon.config.ts
+++ src/app/shared/atoms/button-icon/button-icon.config.ts
@@ -3,7 +3,7 @@ type ButtonIconLike    = 'like';
 type ButtonIconNavbar  = 'home' | 'search' | 'sell' | 'message' | 'profile';
 type ButtonIconSelect  = 'star';
 type ButtonIconSimple  = 'pencil' | 'trash' | 'clip' | 'bell' | 'microphone' | 'photo';
-type ButtonIconGeneral = 'arrow-left' | 'arrow-right' | 'arrow-left-circle' | 'arrow-right-circle' 
+type ButtonIconGeneral = 'arrow-left' | 'arrow-right' | 'arrow-left-circle' | 'arrow-right-circle' | 'trash-button' | 'add-photo' | 'add-photo-icon' | 'profile-img' 
 
 export type ButtonIconVariant = ButtonIconLike | ButtonIconNavbar | ButtonIconSelect | ButtonIconSimple | ButtonIconGeneral;
 
