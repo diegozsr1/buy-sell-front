457f7976956008fb308d0e5145dbb53f6af4cd56
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-16 06:52:52 +0200

    maquetar estadísticas

--- src/app/components/statistics/statistics.ts
+++ src/app/components/statistics/statistics.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './statistics.html',
   styleUrl: './statistics.css',
 })
-export class Statistics {
 
+export class Statistics implements AfterViewInit {
+
+  ngAfterViewInit() {
+    new Chart('salesChart', {
+      type: 'bar',
+      data: {
+        labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun'],
+        datasets: [{
+          label: 'Ventas',
+          data: [1200, 1800, 1500, 2200, 2800, 3100]
+        }]
+      }
+    });
+  }
 }
