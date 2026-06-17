d64b7bc5e429ee24db429255235afed2ae94e517
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-17 17:37:53 +0200

    fix(organism): fix commit merge conflict

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './shared/organisms/header/header';
-import { Footer } from './shared/organisms/footer/footer';
-import { PreFooter } from './shared/organisms/pre-footer/pre-footer';
+import { Header } from './shared/header/header';
+import { MobileBar } from './shared/mobile-bar/mobile-bar';
+import { Footer } from './shared/footer/footer';
+import { PreFooter } from './shared/pre-footer/pre-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, PreFooter],
+  imports: [RouterOutlet, Header, Footer, PreFooter, MobileBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
