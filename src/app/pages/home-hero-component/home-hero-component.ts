63ed35c223a1de064cbca19c8d5127e1156d70ca
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-18 17:19:38 +0200

    maquetar home-hero en mobile

--- src/app/pages/home-hero-component/home-hero-component.ts
+++ src/app/pages/home-hero-component/home-hero-component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { NLInicioHero } from '../../components/no_logueado/nl-inicio-hero/nl-inicio-hero';
 import { NLInicio } from '../../components/no_logueado/nl-inicio/nl-inicio';
 import { Heading } from '../../components/no_logueado/heading/heading';
+import { HomeBar } from '../../shared/home-bar/home-bar';
+import { Buscador } from '../../shared/buscador/buscador';
 
 @Component({
   selector: 'app-home-hero-component',
-  imports: [NLInicioHero,NLInicio,Heading],
+  imports: [NLInicioHero,NLInicio,Heading,HomeBar,Buscador],
   templateUrl: './home-hero-component.html',
   styleUrl: './home-hero-component.css',
 })
 export class HomeHeroComponent {
   user:any={};
+  textoBusqueda:string='';
+  placeholder:string='Buscar movil, portatil, tablet...';
 
   ngOnInit(){
     const usuarioString = localStorage.getItem('usuarioBuy&Sell');
