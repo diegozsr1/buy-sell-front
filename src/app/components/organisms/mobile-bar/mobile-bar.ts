0763744ef592f4406a17206730ffaa23dcbedce9
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-18 19:51:07 +0200

    feat(mobile-bar): integrate link-icons atoms components  to mobile-bar organism

--- src/app/components/organisms/mobile-bar/mobile-bar.ts
+++ src/app/components/organisms/mobile-bar/mobile-bar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { ButtonIcon } from '../../atoms/button-icon/button-icon';
+import { LinkIcon } from '../../atoms/link-icon/link-icon';
 
 @Component({
   selector: 'app-mobile-bar',
-  imports: [RouterLink, RouterLinkActive, ButtonIcon],
+  imports: [LinkIcon],
   templateUrl: './mobile-bar.html',
   styleUrl: './mobile-bar.css',
 })
