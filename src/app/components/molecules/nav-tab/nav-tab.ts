3897adaebd19fc1a4aeab5e21a012960097f14d7
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-23 23:42:04 +0200

    feat(nav-tab): generate nav-tab molecule component

---
+++ src/app/components/molecules/nav-tab/nav-tab.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-tab',
+  imports: [],
+  templateUrl: './nav-tab.html',
+  styleUrl: './nav-tab.css',
+})
+export class NavTab {
+
+}
