def37956866b1062370d9f05779cd43dd04e8eae
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-23 17:07:18 +0200

    feat(user-contact): program stylesheet and template of user contact for desktop and mobile

--- src/app/components/molecules/user-contact/user-contact.ts
+++ src/app/components/molecules/user-contact/user-contact.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Icon } from '../../atoms/icon/icon';
+import { Badge } from '../../atoms/badge/badge';
+import { Button } from '../../atoms/button/button';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'molecule-user-contact',
-  imports: [],
+  imports: [RouterLink, Icon, Badge, Button, RouterLink],
   templateUrl: './user-contact.html',
   styleUrl: './user-contact.css',
 })
 export class UserContact {
+
+  private router = inject(Router);
+
+  protected contactUser() {
+    this.router.navigate(['/user']);
+  }
 
 }
