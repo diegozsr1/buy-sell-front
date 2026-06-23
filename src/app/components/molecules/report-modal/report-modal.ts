f7fef3e8e141089481fc80674d10fc75eaaf27ad
diegozsr1 <diegozsr1@gmail.com>
2026-06-23 10:11:50 +0200

    Componentes creados

---
+++ src/app/components/molecules/report-modal/report-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-modal',
+  imports: [],
+  templateUrl: './report-modal.html',
+  styleUrl: './report-modal.css',
+})
+export class ReportModal {
+
+}
---
+++ src/app/pages/reports/product-report-page/product-report-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-report-page',
+  imports: [],
+  templateUrl: './product-report-page.html',
+  styleUrl: './product-report-page.css',
+})
+export class ProductReportPage {
+
+}
