bba42b322b822b46001d0b04d2488040c79282e8
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-24 16:34:25 +0200

    feat(article-card): create minimal card, desktop and mobile format

--- src/app/components/molecules/cards/article-card/article-card.ts
+++ src/app/components/molecules/cards/article-card/article-card.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ButtonIcon } from '../../../atoms/button-icon/button-icon';
+import { Badge } from '../../../atoms/badge/badge';
+import { BadgeCondition } from '../../../atoms/badge/badge.types';
+import { ArticleCardVariant } from './article-card.config';
 
 @Component({
-  selector: 'app-article-card',
-  imports: [],
+  selector: 'molecule-article-card',
+  imports: [Badge, ButtonIcon],
   templateUrl: './article-card.html',
   styleUrl: './article-card.css',
 })
 export class ArticleCard {
-
+  public variant  = input<ArticleCardVariant>('minimal')
+  public like     = input<boolean>(true);
+  public state    = input<BadgeCondition>('Como nuevo');
+  public img      = input<string>("https://placehold.co/400x200");
+  public price    = input<number>(5070);
+  public name     = input<string>("Article Name");
+  public location = input<string>("Madrid");
+  
 }
