6bb4785cee0f0847d0975bcb7f475f2f706fdbc1
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-25 11:23:12 +0200

    maquetar valorar compra US-19

---
+++ src/app/components/molecules/cards/review-user/review-user.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Icon } from '../../../atoms/icon/icon';
+
+@Component({
+  selector: 'app-review-user',
+  imports: [Icon],
+  templateUrl: './review-user.html',
+  styleUrl: './review-user.css',
+})
+export class ReviewUser {
+
+}
--- src/app/components/organisms/user/write-review/write-review.ts
+++ src/app/components/organisms/user/write-review/write-review.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ReviewUser } from '../../../molecules/cards/review-user/review-user';
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-write-review',
-  imports: [],
+  imports: [ReviewUser, RouterLink, ReactiveFormsModule],
   templateUrl: './write-review.html',
   styleUrl: './write-review.css',
 })
 export class WriteReview {
+  miForm: FormGroup;
+  mensaje: string = '';
+  tipo: boolean = false;
+  valor:number=0;
 
+  constructor(private cd: ChangeDetectorRef) {
+    this.miForm = new FormGroup({
+      comentario: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    }, []);
+  }
+
+  get comentario() {
+    return this.miForm.get('comentario');
+  }
+
+  loadData() {
+    if (!this.miForm.valid) {
+      this.miForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.miForm.value);
+  }
+  pasarDato(valor: number) {
+    this.valor=valor;
+  }
 }
