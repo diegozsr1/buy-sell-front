ef8ca62c6598251c5a5bd82c1a9ef549c0b4b999
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-25 14:01:41 +0200

    feat(user-card): generate component

---
+++ src/app/components/molecules/user-card/user-rating/user-rating.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-rating',
+  imports: [],
+  templateUrl: './user-rating.html',
+  styleUrl: './user-rating.css',
+})
+export class UserRating {
+
+}
