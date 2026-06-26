63c4ff5bb613bc6c6503f6d3f940f5627dbf060c
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-26 23:09:48 +0200

    feat(template): template component page skeleton

--- src/app/pages/product-form-component/product-form-component.component.ts
+++ src/app/pages/product-form-component/product-form-component.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Button } from "../../components/atoms/button/button";
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-product-form-component',
-  imports: [],
+  imports: [Button],
   templateUrl: './product-form-component.component.html',
   styleUrl: './product-form-component.component.css',
 })
 export class ProductFormComponentComponent {
+  public readonly QUERYPARAM_DETAIL:   string = "detail"
+  public readonly QUERYPARAM_PRICE:    string = "price"
+  public readonly QUERYPARAM_PICTURES: string = "pictures"
+  
+   //Services
+  private router = inject(Router);
+   private actived_route = inject(ActivatedRoute);
 
+  ngOnInit() { 
+    this.router.navigate([], {
+        queryParams: { tab: this.QUERYPARAM_DETAIL },
+        queryParamsHandling: 'merge'  // conserva otros query params existentes
+    })
+  }
+
+
+  protected activeTab = toSignal(
+    this.actived_route.queryParamMap.pipe(
+        map(params => params.get('tab'))
+    ));
 }
