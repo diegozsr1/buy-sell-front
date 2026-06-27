e081eb80fceb3b0e2559cc34d7eaef9660519e9f
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-27 18:25:43 +0200

    feat(nav-step): update nav-step with route param query

--- src/app/components/organisms/navs/nav-step/nav-step.ts
+++ src/app/components/organisms/navs/nav-step/nav-step.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { NavStepOptions } from './nav-step.config';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'organism-nav-step',
@@ -10,5 +12,25 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class NavStep {
   public steps      = input<NavStepOptions[]>([{name:"1",label:"DETALLES",query_param:"detail"}, {name:"2",label:"PRECIO",query_param:"price"},{name:"3",label:"FOTOS",query_param:"photo"}]);
-  public activeStep = input<string>('');
+  public stack      = input<boolean>(false);
+
+  public nextStep = input(0)
+  public previousStep = input(0)
+
+  private activatedRoute = inject(ActivatedRoute);
+  
+  protected activeSteps = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map(params => {
+        const parameters = params.getAll('step')
+        return parameters;
+      })
+    ),
+    { initialValue: [] as string[] }
+  );
+
+  protected isStepActive(query_param: string): boolean {
+    console.log(this.activeSteps().includes(query_param))
+    return this.activeSteps().includes(query_param);
+  }
 }
