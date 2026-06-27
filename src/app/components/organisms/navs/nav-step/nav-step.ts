6be9ded0b6d78991f72e1eca954a883fe617f3a1
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-27 16:34:49 +0200

    feat(nav-step): add nav-step style and template

---
+++ src/app/components/organisms/navs/nav-step/nav-step.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { NavStepOptions } from './nav-step.config';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'organism-nav-step',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './nav-step.html',
+  styleUrl: './nav-step.css',
+})
+export class NavStep {
+  public steps      = input<NavStepOptions[]>([{name:"1",label:"DETALLES",query_param:"detail"}, {name:"2",label:"PRECIO",query_param:"price"}]);
+  public activeStep = input<string>('');
+}
