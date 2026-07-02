c5c848854bd0234c9b25ffa264198c51ccc9c964
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-07-02 19:21:35 +0200

    refactor(new-product): put detail-form, price-form and phto-uploader to  organism/forms directory

--- src/app/components/organisms/forms/price-form/price-form.ts
+++ src/app/components/organisms/forms/price-form/price-form.ts
@@ -1,5 +1,6 @@
 import { Component, effect, input, OnInit, output } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IArticlePrice } from '../../../../interfaces/i-article';
 
 
 @Component({
@@ -12,10 +13,10 @@ export class PriceForm implements OnInit {
  showErrors = input<number>(0);
  
  formValid = output<boolean>();
+ formValue = output<IArticlePrice>();
 
   priceForm = new FormGroup({
-    precio: new FormControl<number | null>(null, [Validators.required, Validators.min(1)]),
-    ubicacion: new FormControl('', Validators.required),
+    precio: new FormControl<number>(0, {nonNullable: true,validators:[Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]}),
   });
 
 
@@ -30,9 +31,16 @@ export class PriceForm implements OnInit {
 
   ngOnInit(): void {
     this.formValid.emit(this.priceForm.valid);
+    this.formValue.emit(this.priceForm.getRawValue());
+
     this.priceForm.statusChanges.subscribe(() => {
       this.formValid.emit(this.priceForm.valid);
     });
+
+    this.priceForm.statusChanges.subscribe(() => {
+      this.formValue.emit(this.priceForm.getRawValue());
+    });
+
   }
 
   markAllAsTouched(): void {
